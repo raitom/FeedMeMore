ce2f916936490da0e4123837ae993b24a86b38b8
Thomas Geselle <raitomm@gmail.com>
2013-09-17 21:01:06 +0200

    Rails 3.2.14 tout frais

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Feedmemore::Application.config.secret_token = '6d3a17602590ae15195d11287fa2c7892679612f962012b988f32b150cb8607f5c9be91d8c1c0ebddbab10d447b0105d692860b392a4b40a82b91efb7f09fc41'
